5173fd938e328dff06a8297c1e39064cc662db18
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-09-27 11:08:13 +0000

    Edited

--- src/core/index.js
+++ src/core/index.js
@@ -1,2 +1,2 @@
-import '../components/example-component/example-component.js'
+// import '../components/example-component/example-component.js'
 import '../components/example-component-2/example-component-2.js'
